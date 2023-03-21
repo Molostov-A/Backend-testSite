530f5a0defb954ab61f97e1a6fd2f457bfabf144
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2023-03-21 17:28:42 +0300

    Add UnitTests

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
